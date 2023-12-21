73682e26012ea8e7349eb53acebc3d07f0bed357
Kaua Gabriell <kg3guarino@gmail.com>
2023-12-21 13:14:08 -0300

    Javascript atualizado e tamanho reduzido

--- script.js
+++ script.js
@@ -1,38 +1,11 @@
-const userInput = document.getElementById('user');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
-
-userInput.addEventListener('keypress', (event) => {
+window.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
-    const value = userInput.value;
+    const value = event.target.value;
+    const elementTarget = event.target;
     if (value == false) {
-      userInput.style.outline = '1px solid red';
+      elementTarget.style.outline = '1px solid red';
     } else {
-      userInput.style.outline = 'none';
-    }
-  }
-});
-
-if (emailInput != undefined) {
-  emailInput.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-      const value = emailInput.value;
-      if (value == false) {
-        emailInput.style.outline = '1px solid red';
-      } else {
-        emailInput.style.outline = 'none';
-      }
-    }
-  });
-}
-
-passwordInput.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    const value = passwordInput.value;
-    if (value == false) {
-      passwordInput.style.outline = '1px solid red';
-    } else {
-      passwordInput.style.outline = 'none';
+      elementTarget.style.outline = 'none';
     }
   }
 });
